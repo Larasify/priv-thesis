53a91ff40c017f24097fd11e77905607ec3c792c
Larasify <lara.cikis@gmail.com>
2023-08-04 15:52:07 +0300

    shell commands

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,12 +2,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  files: string[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log("hey");
+
+  var shell = require('shelljs');
+  shell.cd('src');
+  shell.cd('pages');
+  
+  shell.echo('hello world');
+  //console.log(shell.ls()[0]);
+  const files:string[] = []
+  shell.ls().forEach(function (file: string) {
+    console.log(file);
+    files.push(file);
+  });
+
+  //return files array as json
+  res.status(200).json({ files: files })
+
+  //res.status(200).json({ name: 'John Doe' })
 }
